9ecc38d78f4f3bb32cd22069593fd151e94a20d8
aihamjarrah <ayham.mjarr@gmail.com>
2021-01-01 22:25:44 +0200

    Moved to Part-4

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -11,26 +11,34 @@ Q2) Name three types of relationships in databases.
 
 
 Q3) What are the main three sections of a JWT?
-
+1) 
+2) Payload
+3) Signature
 
 Q4) Name three types of express middle-wares.
-
+1) Application-level middlewares
+2) Router-level middlewares
+3) 
 
 Q5) What is the `effect` hook used for in react?
-
+It is used to perform an action when an event happens 
 
 Q6) What is the `state` hook used for in react?
-
+It is used to create a state and a function that changes that state
 
 Q7) List three thing you can use to create a responsive web application.
-
+1) Express library 
+2) MYSQL database
+3) axios library 
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
 
 Q9) What is the difference between authentication and authorization?
-
+authentication is the action of verifying the user credintials such as password, token and cards
+authorization is giving the user permessions based on the authentication of the credintials 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
+So that we can use this information in our application whithout providing them to others, the .env file keeps this information private by ignoring this file from the git commit
 
 */
