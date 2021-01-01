9e252818486a1ecd15d9b141aca2a790e6503f27
aihamjarrah <ayham.mjarr@gmail.com>
2021-01-01 22:31:17 +0200

    Solved Q4

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -11,8 +11,19 @@ return 'NO' if the parentheses in the input string are not balanced,.
 
 console.log('PART 4');
 
-const isABalancedString = () => {
+const isABalancedString = (str) => {
   // WRITE YOUR CODE BELOW THIS LINE
+  const half_1 = str.slice(str.length/2);
+  const half_2 = str.slice(str.length/2, str.length-1)
+  const half_2_reversed = half_2.split("").reverse().join("")
+  if(half_1 === half_2_reversed){
+    return 'YES'
+  }
+  else{
+    return 'NO'
+  }
+
+
 };
 
 /*
